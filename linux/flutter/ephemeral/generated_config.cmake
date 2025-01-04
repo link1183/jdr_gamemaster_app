cd6d9e0b932925212a34c9ad6aae74d677e8cd80
link1183 <adrien.gunther@gmail.com>
2025-01-04 23:42:15 +0000

    Fix: Updated gitignore to include linux and windows folders

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/agunthe1/.cache/flutter_sdk" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/agunthe1/projects/github/jdr_gamemaster_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/agunthe1/.cache/flutter_sdk"
+  "PROJECT_DIR=/home/agunthe1/projects/github/jdr_gamemaster_app"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/agunthe1/projects/github/jdr_gamemaster_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/agunthe1/projects/github/jdr_gamemaster_app/lib/main.dart"
+)
